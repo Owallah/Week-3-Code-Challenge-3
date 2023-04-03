71dadb0672fcdbd0e4f34e5690e15bea4f1a3af6
Johnson <mikesonowallah@gmail.com>
2023-04-03 13:16:43 +0300

    added a div to display one movie item

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', (e) => {
     e.preventDefault()
+    firstMovie()
     fetchMovies()
 })
 
@@ -14,6 +15,36 @@ function fetchMovies() {
     .then(movies => renderMovies(movies))
     .catch(error => console.error(error.message))
 
+}
+const firstMovie = () => {
+    return fetch('http://localhost:3000/films/1', {
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+    })
+    .then(response => response.json())
+    .then(movie => renderFirstMovie(movie))
+    .catch(error => console.error(error.message))
+}
+
+const renderFirstMovie = (movie) => {
+    let firstMovie = document.querySelector('.first-movie')
+    let availableTickets = movie.capacity - movie.tickets_sold
+
+    firstMovie.innerHTML = `
+    <div class="first-movie-card">
+        <div class="first-movie-card-image"><img src="${movie.poster}" ></div>
+        <div class="first-movie-title"> ${movie.title} </div>
+        <div class="first-movie-description"> ${movie.description}
+            <div class="first-movie-runtime"> Runtime: <span class="name">${movie.runtime}</span>mins</div>
+            <div class="first-movie-showtime"> Showtime: <span class="name">${movie.showtime}</span></div>
+        </div>
+        <h4 id = 'tickets'>Available Tickets: <span class = 'available-tickets'>${availableTickets}</span></h4>
+        <div class = 'ticket-btn'>
+            <button id="book-ticket">Book Ticket</button>
+        </div>
+    </div>`
 }
 
 const renderMovies = function (movies) {
@@ -35,6 +66,10 @@ const renderMovies = function (movies) {
                 <button id="book-ticket">Book Ticket</button>
             </div>
         </div>`
+
+        // document.querySelector('#book-ticket').addEventListener('click', () => {
+        //     console.log('clicked');
+        // })
 
         main.appendChild(movieCard)
     });
